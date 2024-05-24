ca6a5db69f014b1762713bb69a1b8b037c6dfefd
Rick Bosch <emerick.bosch@gmail.com>
2024-05-24 19:45:44 +0200

    Initial commit

---
+++ src/main/java/com/xpcoffee/rock_paper_scissors/RockPaperScissorsApplication.java
@@ -0,0 +1,13 @@
+package com.xpcoffee.rock_paper_scissors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockPaperScissorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockPaperScissorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xpcoffee/rock_paper_scissors/RockPaperScissorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpcoffee.rock_paper_scissors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockPaperScissorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
